6087409d626407815afba53b6f251e42d12efb4f
Valentin Naboka <valentin.naboka@gmail.com>
2021-03-25 00:27:09 +0200

    Extract config to env variables

--- main.go
+++ main.go
@@ -5,11 +5,36 @@ import (
 	"fmt"
 	"heatmeter/mbus"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
+	var device, mbusIDVar, baudrateVar string
+	if device := os.Getenv("HM_DEVICE"); device == "" {
+		log.Fatal("HM_DEVICE variable is not set")
+	}
+
+	if mbusIDVar = os.Getenv("HM_MBUS_ID"); mbusIDVar == "" {
+		log.Fatal("HM_MBUS_ID variable is not set")
+	}
+
+	mbusID, err := strconv.Atoi(mbusIDVar)
+	if err != nil{
+		log.Fatalf("Wrong mbus ID: %s, %s", mbusIDVar, err)
+	}
+
+	if baudrateVar=os.Getenv("HM_BAUDRATE"); baudrateVar == "" {
+		log.Fatal("HM_BAUDRATE variable is not set")
+	}
+
+	baudrate, err := strconv.Atoi(baudrateVar)
+	if err != nil{
+		log.Fatalf("Wrong baudrate: %s, %s", baudrateVar, err)
+	}
+
 	var reader mbus.Reader
-	err := reader.Open("/dev/cu.usbserial-1410", 13, 2400)
+	err = reader.Open(device, uint8(mbusID), uint16(baudrate))
 	defer reader.Close()
 	if err != nil {
 		log.Fatal(err)
